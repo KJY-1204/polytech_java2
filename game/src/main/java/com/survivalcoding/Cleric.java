57962b6d3d5f13e0e20ec72fe05bf453a4e425d6
KJY <dbvcxz@gmail.com>
2026-06-08 11:51:57 +0900

    연습문제 1

---
+++ game/src/main/java/com/survivalcoding/Cleric.java
@@ -0,0 +1,9 @@
+package com.survivalcoding;
+
+public class Cleric {
+    final int MAXHP = 50;
+    final int MAXMP = 10;
+    String name;
+    int HP = 50;
+    int MP = 10;
+}
---
+++ game/src/main/java/com/survivalcoding/Main.java
@@ -0,0 +1,11 @@
+package com.survivalcoding;
+
+public class Main {
+    public static void main(String[] args) {
+        //인스턴스 생성
+        Hero hero = new Hero();
+
+        int a = 10;
+        long l = 10L; //literal 리터널
+    }
+}

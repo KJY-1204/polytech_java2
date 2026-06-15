8682d0ba3f3afbe499bc3e3372ae81eae7229a9d
KJY <dbvcxz@gmail.com>
2026-06-15 13:28:18 +0900

    20_06_15 캡슐화 연습문제 1-2

--- game/src/main/java/com/survivalcoding/Wand.java
+++ game/src/main/java/com/survivalcoding/Wand.java
@@ -9,6 +9,12 @@ public class Wand {
     }
 
     public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("지팡이의 이름은 null일 수 없습니다.");
+        }
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("지팡이의 이름은 반드시 3문자 이상이어야 한다.");
+        }
         this.name = name;
     }
 
@@ -17,6 +23,9 @@ public class Wand {
     }
 
     public void setPower(double power) {
+        if (power < 0.5 || power > 100.0) {
+            throw new IllegalArgumentException("지팡이의 마력은 0.5이상 100.0이하여야 합니다.");
+        }
         this.power = power;
     }
 }
--- game/src/main/java/com/survivalcoding/Wizard.java
+++ game/src/main/java/com/survivalcoding/Wizard.java
@@ -1,7 +1,7 @@
 package com.survivalcoding;
 
 public class Wizard {
-    Wand wand;
+    private Wand wand;
     private int hp;
     private int mp;
     private String name;
@@ -12,12 +12,27 @@ public class Wizard {
         hero.setHP(hero.getHP() + recovPoint);
     }
 
+    public Wand getWand() {
+        return wand;
+    }
+
+    public void setWand(Wand wand) {
+        if (wand == null) {
+            throw new IllegalArgumentException("마법사의 지팡이는 null 일 수 없다.");
+        }
+        this.wand = wand;
+    }
+
     public int getHp() {
         return hp;
     }
 
     public void setHp(int hp) {
-        this.hp = hp;
+        if (hp < 0) {
+            this.hp = 0;
+        } else {
+            this.hp = hp;
+        }
     }
 
     public int getMp() {
@@ -25,6 +40,9 @@ public class Wizard {
     }
 
     public void setMp(int mp) {
+        if (mp < 0) {
+            throw new IllegalArgumentException("마법사의 MP는 0 이상이어야 한다.");
+        }
         this.mp = mp;
     }
 
@@ -33,6 +51,12 @@ public class Wizard {
     }
 
     public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("마법사의 이름은 null일 수 없습니다.");
+        }
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("마법사의 이름은 반드시 3문자 이상이어야 한다.");
+        }
         this.name = name;
     }
 }

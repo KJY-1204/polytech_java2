bf0e564186f0072f673f58fa8e7868d2c36b936b
KJY <dbvcxz@gmail.com>
2026-06-16 14:48:07 +0900

    26_06_16 상속 연습문제 5

--- game/src/main/java/com/survivalcoding/Hero.java
+++ game/src/main/java/com/survivalcoding/Hero.java
@@ -1,8 +1,9 @@
 package com.survivalcoding;
 
 public class Hero {
-    String name;
-    int HP;
+    private String name;
+    private int HP;
+    private int MaxHP = 100;
 
     public Hero(String name, int HP) {
         this.name = name;
@@ -20,5 +21,21 @@ public class Hero {
     void attack() {
         //sout
         System.out.println("공격했다");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxHP() {
+        return MaxHP;
+    }
+
+    public void setMaxHP(int maxHP) {
+        MaxHP = maxHP;
     }
 }

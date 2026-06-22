2c8c69a2dc2466da5b9658f126bc6d0b170c1208
KJY <dbvcxz@gmail.com>
2026-06-22 14:13:41 +0900

    26_06_22 추상클래스와 인터페이스 연습문제 4

--- game/src/main/java/com/survivalcoding/TangibleAsset.java
+++ game/src/main/java/com/survivalcoding/TangibleAsset.java
@@ -1,14 +1,25 @@
 package com.survivalcoding;
 
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing {
 
     private String color;
+    private double weight;
 
-    public TangibleAsset(String name, int price, String color) {
+    public TangibleAsset(String name, int price, String color, double weight) {
         super(name, price);
         this.color = color;
+        this.weight = weight;
     }
 
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 
     public String getColor() {
         return color;

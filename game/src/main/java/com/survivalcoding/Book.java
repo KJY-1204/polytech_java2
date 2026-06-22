b9f8ce1f653603229e21cadfb279be9a409cb715
KJY <dbvcxz@gmail.com>
2026-06-22 14:17:18 +0900

    26_06_22 추상클래스와 인터페이스 연습문제 4

--- game/src/main/java/com/survivalcoding/Book.java
+++ game/src/main/java/com/survivalcoding/Book.java
@@ -4,8 +4,8 @@ public class Book extends TangibleAsset {
 
     private String isbn;
 
-    public Book(String name, int price, String color, String isbn) {
-        super(name, price, color);
+    public Book(String name, int price, String color, String isbn, double weight) {
+        super(name, price, color, weight);
         this.isbn = isbn;
     }
 
--- game/src/main/java/com/survivalcoding/Computer.java
+++ game/src/main/java/com/survivalcoding/Computer.java
@@ -4,8 +4,8 @@ public class Computer extends TangibleAsset {
 
     private String makerName;
 
-    public Computer(String name, int price, String color, String makerName) {
-        super(name, price, color);
+    public Computer(String name, int price, String color, String makerName, double weight) {
+        super(name, price, color, weight);
         this.makerName = makerName;
     }
 
